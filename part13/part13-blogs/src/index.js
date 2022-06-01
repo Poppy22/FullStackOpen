93b12ea44bf5ba15d6ba341d5473848862e7559c
Poppy22 <carmengpopa13@gmail.com>
2022-06-01 21:10:33 +0300

    part 13a

---
+++ part13/part13-blogs/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const { PORT } = require('./utils/config')
+const { connectToDatabase } = require('./utils/db')
+
+const blogsRouter = require('./controllers/blog')
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
